4b2cf0510a736d2e1a62b8f7f2c19f924d911d79
lauraservat <lservaba21@alumnes.ub.edu>
2025-11-02 12:37:03 +0100

    Formulari creat

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,67 +1,65 @@
-import { createRouter, createWebHistory } from "vue-router";
-import LogIn from "../views/LogIn.vue";
-import SignUp from "../views/SignUp.vue";
-import Home from "../views/Home.vue";
-import Search from "../views/Search.vue";
-import User from "../views/User.vue";
-import SongDetailById from "../views/SongDetailById.vue";
-import Playlists from "../views/Playlists.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import LogIn from '../views/LogIn.vue'
+import SignUp from '../views/SignUp.vue'
+import Home from '../views/Home.vue'
+import Search from '../views/Search.vue'
+import User from '../views/User.vue'
+import SongDetailById from '../views/SongDetailById.vue'
+import Playlists from '../views/Playlists.vue'
 
-import { useAuthStore } from "../store/authStore";
+import { useAuthStore } from '../store/authStore'
 
 const routes = [
   {
-    path: "/login",
-    name: "logIn",
+    path: '/login',
+    name: 'logIn',
     component: LogIn,
+    meta: { hideChrome: true },
   },
   {
-    path: "/SignUp",
-    name: "sign_up",
+    path: '/SignUp',
+    name: 'sign_up',
     component: SignUp,
   },
-    {
-    path: "/",
-    name: "home",
+  {
+    path: '/',
+    name: 'home',
     component: Home,
   },
-  { path: '/search',
-    name: "search",
-    component: Search },
+  { path: '/search', name: 'search', component: Search },
   {
-    path: "/user/:username",
-    name: "user",
+    path: '/user/:username',
+    name: 'user',
     component: User,
   },
   {
-    path: "/song/:id(\\d+)",
-    name: "song-by-id",
+    path: '/song/:id(\\d+)',
+    name: 'song-by-id',
     component: SongDetailById,
-    props: true
+    props: true,
   },
   {
-    path: "/playlist/:id",
-    name: "playlist",
+    path: '/playlist/:id',
+    name: 'playlist',
     component: Playlists,
     props: true,
   },
-
-];
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+})
 
 router.beforeEach((to, from, next) => {
-  const authStore = useAuthStore();
-  authStore.initializeAuthStore(); // Ensure state is up-to-date
+  const authStore = useAuthStore()
+  authStore.initializeAuthStore() // Ensure state is up-to-date
 
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-    next({ name: "catalog" });
+    next({ name: 'catalog' })
   } else {
-    next();
+    next()
   }
-});
+})
 
-export default router;
+export default router
