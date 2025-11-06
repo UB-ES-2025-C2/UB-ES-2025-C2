a4ae46cb573f162e9a22193df0f4bfc0d372373e
lauraservat <lservaba21@alumnes.ub.edu>
2025-11-06 13:36:22 +0100

    Header canvia quan inicies sessió

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,130 +1,120 @@
 <script setup>
-import { ref, onMounted, computed  } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { useAuthStore } from "../store/authStore";
-import {useApiStore } from "../store/guestApi.js";
+import { ref, onMounted, computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useAuthStore } from '../store/authStore'
+import { useApiStore } from '../store/guestApi.js'
 
-const authStore = useAuthStore();
-const apiStore = useApiStore();
-const username = ref("");
+const authStore = useAuthStore()
+const apiStore = useApiStore()
+const username = ref('')
 
-const route = useRoute();
-const router = useRouter();
+const route = useRoute()
+const router = useRouter()
 
-const nusers = ref("");
+const nusers = ref('')
 
 const buscarUsuari = async () => {
-  await apiStore.getUser(username.value);
-};
-const buscarNUsers = async () => {
-  await apiStore.searchNUsers(nusers.value);
-};
-function viewprofile() {
-  const myid = authStore.user_id;
-  router.push({ name: "profile" , params: { id: myid } });
+  await apiStore.getUser(username.value)
 }
-
+const buscarNUsers = async () => {
+  await apiStore.searchNUsers(nusers.value)
+}
+function viewprofile() {
+  const myid = authStore.user_id
+  router.push({ name: 'profile', params: { id: myid } })
+}
 
 /*mock data para playlists de prueba*/
 const mockPlaylists = ref([
   {
     id: 1,
-    name: "Playlist 1",
+    name: 'Playlist 1',
     description: "Playlist d'èxits mundials",
-    topic: "Èxits mundials",
-    cover: "https://marketplace.canva.com/EAEkDXCwwcE/1/0/1600w/canva-playlist-cover-tipogr%C3%A1fico-de-m%C3%BAsica-pop-rosa-rosa-y-t%C3%ADtulo-grande-tonos-arcoiris-NvXdCHt3cJc.jpg"
+    topic: 'Èxits mundials',
+    cover:
+      'https://marketplace.canva.com/EAEkDXCwwcE/1/0/1600w/canva-playlist-cover-tipogr%C3%A1fico-de-m%C3%BAsica-pop-rosa-rosa-y-t%C3%ADtulo-grande-tonos-arcoiris-NvXdCHt3cJc.jpg',
   },
   {
     id: 2,
-    name: "Playlist 2",
-    description: "Playlist de pop",
-    topic: "Pop",
-    cover: "https://marketplace.canva.com/EAGGPj4-B4c/1/0/1600w/canva-portada-para-playlist-deep-house-moderno-violeta-y-rojo-GcfjW55ejVs.jpg"
+    name: 'Playlist 2',
+    description: 'Playlist de pop',
+    topic: 'Pop',
+    cover:
+      'https://marketplace.canva.com/EAGGPj4-B4c/1/0/1600w/canva-portada-para-playlist-deep-house-moderno-violeta-y-rojo-GcfjW55ejVs.jpg',
   },
   {
     id: 3,
-    name: "Playlist3",
-    description: "Playlist de rock",
-    topic: "Rock",
-    cover: "https://marketplace.canva.com/EAEgRCviBys/1/0/1600w/canva-morado-y-rojo-naranja-est%C3%A9tica-de-tumblr-relajante-ac%C3%BAstico-cl%C3%A1sico-lo-fi-portada-de-lista-de-reproducci%C3%B3n-jE51M26tg2g.jpg"
-  }
-]);
+    name: 'Playlist3',
+    description: 'Playlist de rock',
+    topic: 'Rock',
+    cover:
+      'https://marketplace.canva.com/EAEgRCviBys/1/0/1600w/canva-morado-y-rojo-naranja-est%C3%A9tica-de-tumblr-relajante-ac%C3%BAstico-cl%C3%A1sico-lo-fi-portada-de-lista-de-reproducci%C3%B3n-jE51M26tg2g.jpg',
+  },
+])
 
 /* Agrupamos las playlists por topic */
 const playlistsByTopic = computed(() => {
-  const grouped = {};
-  apiStore.playList.forEach(pl => {
-    const topic = pl.topic || "Sense tema";
-    if (!grouped[topic]) grouped[topic] = [];
-    grouped[topic].push(pl);
-  });
-  return grouped;
-});
+  const grouped = {}
+  apiStore.playList.forEach((pl) => {
+    const topic = pl.topic || 'Sense tema'
+    if (!grouped[topic]) grouped[topic] = []
+    grouped[topic].push(pl)
+  })
+  return grouped
+})
 
 onMounted(() => {
-  authStore.initializeAuthStore();
-  apiStore.fetchCatalog();
-  apiStore.fetchPlaylists();
-});
+  authStore.initializeAuthStore()
+  apiStore.fetchCatalog()
+  apiStore.fetchPlaylists()
+})
 
 // Nombre para la cabecera
 const headerUsername = computed(() => {
-  if (authStore.username) return authStore.username; // si está logueado
-  if (apiStore.nUsersResult.length) return apiStore.nUsersResult[0].username; // primer usuario de la API
-  return "Usuari"; // por defecto
-});
-
+  if (authStore.username) return authStore.username // si está logueado
+  if (apiStore.nUsersResult.length) return apiStore.nUsersResult[0].username // primer usuario de la API
+  return 'Usuari' // por defecto
+})
 </script>
 
 <template>
-    <section class="home-header">
-      <p class="made-for">Fet per a</p>
-      <h1 class="username">{{ headerUsername }}</h1>
-      <button @click="viewprofile">View Profile</button>
-    </section>
-
-    <!-- Canciones -->
-    <h2>Cançons</h2>
-    <ul class="cards-list">
-      <li v-for="song in apiStore.songs" :key="song.id" class="card">
-        <!-- Imagen de la canción -->
-        <div class="card-image">
-          <img :src="song.image" alt="Foto de canción"/>
-
-        </div>
-
-        <!-- Info: nombre y artista -->
-        <div class="card-info">
-          <strong>{{ song.name }}</strong>
-          <p>{{ song.artist }}</p>
-        </div>
-
-        <!-- Reproductor de audio -->
-        <audio :src="song.file_audio" controls class="song-audio"></audio>
-
-
-      </li>
-    </ul>
-
-    <!-- Playlists -->
-      <h2>Playlists</h2>
-      <div v-for="(pls, topic) in playlistsByTopic" :key="topic" class="topic-section">
-        <h3>{{ topic }}</h3>
-        <ul class="cards-list">
-          <li v-for="playlist in pls" :key="playlist.id" class="card">
-            <div class="card-image">
-              <img :src="playlist.cover" alt="Foto de playlist" />
-            </div>
-            <div class="card-info">
-              <strong>{{ playlist.name }}</strong>
-              <p class="description">{{ playlist.description }}</p>
-            </div>
-          </li>
-        </ul>
+  <!-- Canciones -->
+  <h2>Cançons</h2>
+  <ul class="cards-list">
+    <li v-for="song in apiStore.songs" :key="song.id" class="card">
+      <!-- Imagen de la canción -->
+      <div class="card-image">
+        <img :src="song.cover" alt="Foto de canción" />
       </div>
 
-</template>
+      <!-- Info: nombre y artista -->
+      <div class="card-info">
+        <strong>{{ song.name }}</strong>
+        <p>{{ song.artist }}</p>
+      </div>
 
+      <!-- Reproductor de audio -->
+      <audio :src="song.file_audio" controls class="song-audio"></audio>
+    </li>
+  </ul>
+
+  <!-- Playlists -->
+  <h2>Playlists</h2>
+  <div v-for="(pls, topic) in playlistsByTopic" :key="topic" class="topic-section">
+    <h3>{{ topic }}</h3>
+    <ul class="cards-list">
+      <li v-for="playlist in pls" :key="playlist.id" class="card">
+        <div class="card-image">
+          <img :src="playlist.cover" alt="Foto de playlist" />
+        </div>
+        <div class="card-info">
+          <strong>{{ playlist.name }}</strong>
+          <p class="description">{{ playlist.description }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
 
 <style scoped>
 .cards-list {
@@ -186,5 +176,4 @@ const headerUsername = computed(() => {
   color: white;
   margin: 0;
 }
-
 </style>
