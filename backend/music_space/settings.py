eac5450905c1cf8a2287fa752b03d710563db0b1
Mastonmo <126565271+Mastonmo@users.noreply.github.com>
2025-11-11 16:04:33 +0100

    hotfix docker

--- backend/music_space/settings.py
+++ backend/music_space/settings.py
@@ -15,12 +15,10 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR.parent / ".env")  # Carrega les variables del .env
 
 
 # Quick-start development settings - unsuitable for production
