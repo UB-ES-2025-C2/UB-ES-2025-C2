ed80aa6950ab7b2c26a98f763bc80c0eb13754e7
Mastonmo <126565271+Mastonmo@users.noreply.github.com>
2025-11-18 16:41:28 +0100

    Add dependencies and read databaseURL

--- backend/music_space/settings.py
+++ backend/music_space/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from datetime import timedelta
 from pathlib import Path
@@ -86,15 +87,19 @@ WSGI_APPLICATION = 'music_space.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        "TEST": {
-            "NAME": BASE_DIR / "test_db.sqlite3",
-        },
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=False)
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+            "TEST": {"NAME": BASE_DIR / "test_db.sqlite3"},
+        }
+    }
 
 
 # Password validation
