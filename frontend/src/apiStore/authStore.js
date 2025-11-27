a1029b00d8af2edb6b2dd52bf8300341221bde08
Flavià Serret Martí <114077503+flaviaserret04@users.noreply.github.com>
2025-11-27 21:11:12 +0100

    comment

--- frontend/src/apiStore/authStore.js
+++ frontend/src/apiStore/authStore.js
@@ -122,7 +122,7 @@ export const useAuthStore = defineStore("auth", {
 
       if (playlist.cover) {
         formData.append("cover", playlist.cover);
-      }
+      } // cover
       return AuthService.postPlaylist(formData);
     }
   }
