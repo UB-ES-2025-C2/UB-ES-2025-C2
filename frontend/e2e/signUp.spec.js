481ea230a9b521e88cb1b2e4cbf083a402e42601
lauraservat <lservaba21@alumnes.ub.edu>
2025-12-13 18:22:22 +0100

    test SIgnUp passa

--- frontend/e2e/signUp.spec.js
+++ frontend/e2e/signUp.spec.js
@@ -1,73 +1,70 @@
-import { expect } from '@playwright/test'
-import { test } from './fixtures/testUser.js'
+import { test, expect } from '@playwright/test'
 
 const BASE_URL = process.env.FRONTEND_URL || 'http://localhost:5173'
 
 test.describe('Registro de usuario', () => {
 
+  const fakeUser = {
+    username: 'usuari_test',
+    password: 'password123Segur!'
+  }
+
   test('Falla si no rellenas todos los campos', async ({ page }) => {
     await page.goto(`${BASE_URL}/SignUp`)
 
-    // Click sin rellenar nada
-    await page.click('button:has-text("Registra\'t")')
+    // Afegeixo { force: true } per arreglar el timeout de Webkit
+    await page.click('button:has-text("Registra\'t")', { force: true })
 
-    // Debe aparecer mensaje de error
     const errorMsg = page.locator('.form-error', { hasText: "Has d'omplir tots els camps" })
     await expect(errorMsg).toBeVisible()
   })
 
-
-  test('Registrar-se: registre correcte', async ({ page, testUser }) => {  const { username, password } = testUser
+  test('Registrar-se: registre correcte', async ({ page }) => {
     await page.goto(`${BASE_URL}/SignUp`)
 
     const newEmail = `nou_${Date.now()}@mail.com`
-    const newUsername = username + '_nou'
+    const newUsername = fakeUser.username + Date.now()
 
     await page.waitForSelector('#username')
     await page.fill('#username', newUsername)
     await page.fill('#email', newEmail)
-    await page.fill('#password', password)
-    await page.fill('#password_conf', password)
+    await page.fill('#password', fakeUser.password)
+    await page.fill('#password_conf', fakeUser.password)
 
-    // Click en el botó de registre
     await page.click('button:has-text("Registra\'t")')
 
     // Esperar la redirección suave
-    await page.waitForURL(`${BASE_URL}/login`)
+    await page.waitForURL(/.*login/)
     await expect(page.locator('button:has-text("Iniciar Sessió")')).toBeVisible()
   })
 
-  test('Correo electrónico inválido', async ({ page, testUser }) => {
-    const { username, password } = testUser
+  test('Correo electrónico inválido', async ({ page }) => {
     await page.goto(`${BASE_URL}/SignUp`)
 
-    await page.fill('#username', username + '_nou')
+    await page.fill('#username', fakeUser.username + '_invalid')
     await page.fill('#email', 'correo-invalido')
-    await page.fill('#password', password)
-    await page.fill('#password_conf', password)
+    await page.fill('#password', fakeUser.password)
+    await page.fill('#password_conf', fakeUser.password)
+
     await page.click('button:has-text("Registra\'t")')
 
-    // Debe aparecer mensaje de error de email
     const errorMsg = page.locator('.form-error', { hasText: 'El correu electrònic no és vàlid' })
     await expect(errorMsg).toBeVisible()
   })
 
-  test('Contraseñas no coinciden mientras rellenas', async ({ page, testUser }) => {
-    const { username, password } = testUser
+  test('Contraseñas no coinciden mientras rellenas', async ({ page }) => {
     await page.goto(`${BASE_URL}/SignUp`)
 
-    await page.fill('#username', username + '_nou')
+    await page.fill('#username', fakeUser.username + '_pwd')
     await page.fill('#email', `nou_${Date.now()}@mail.com`)
-    await page.fill('#password', password)
+    await page.fill('#password', fakeUser.password)
 
-    // Empezar a rellenar la segunda contraseña
     await page.fill('#password_conf', 'otra')
     const errorMsg = page.locator('.error', { hasText: 'Les contrasenyes no coincideixen.' })
     await expect(errorMsg).toBeVisible()
 
-    // Ahora que coinciden
-    await page.fill('#password_conf', password)
-    await expect(errorMsg).toHaveCount(0) // desaparece
+    await page.fill('#password_conf', fakeUser.password)
+    await expect(errorMsg).toHaveCount(0)
   })
 
 })
